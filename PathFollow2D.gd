e12cfbda711fe2dd22025b1da513d79dc913bd2f
Bernat <bernatferrerm@gmail.com>
2019-05-11 21:11:10 +0200

    "AcutalitzacionS : Enemic, path , orientació animació."

---
+++ PathFollow2D.gd
@@ -0,0 +1,25 @@
+extends PathFollow2D
+
+export(int) var speed
+var old_position = Vector2(0,0)
+
+func _process(delta):
+	# Move along the path
+	self.offset +=speed*delta
+	# Detect direction
+	var alpha = rad2deg((self.position - old_position).angle())
+	var new_animation
+	if alpha < -135:
+		new_animation = "left"
+	elif alpha < -45:
+		new_animation = "top"
+	elif alpha < 45:
+		new_animation = "right"
+	elif alpha <135:
+		new_animation = "bottom"
+	else:
+		new_animation = "left"
+	if self.get_children()[0].animation != new_animation:
+		self.get_children()[0].animation = new_animation
+	# Save current position
+	old_position = self.position
