840238043828586015ce7a6a5f418178bc21ff2c
Bernat <bernatferrerm@gmail.com>
2019-05-13 17:44:34 +0200

    " Area endpath , que fa desapareixer tot el que arribi al final del camí"

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,8 +1,33 @@
 extends Node
 
-var enemy = preload("res://Scenes/Skeleton.tscn")
 
+func _rafaga(Enemy,N_ene,t_ene,N_ole,t_ole):
+	
+	"""
+	Retorna  N_ole conjunts de Enemy, amb N_ene unitats
+	per conjunt, cada unitat separada un interval t_ene 
+	i cada conjunt separat un temps t_ole
+	
+	Arguments
+	
+	Enemy: escena la qual conté l'enemic 
+	N_ene: nombre d'enemics que es vol per oleada
+	t_ene: temps concorregut entre cada enemic ( segons)
+	N_ole: Nombre d'oleadas
+	t_ole: temps entre oleada
+	"""
+
+	for i in range(0,N_ole):
+		
+		for j in range(0,N_ene):
+		
+		 $Path2D.add_child(Enemy.instance())
+		
+		 yield(get_tree().create_timer(t_ene),"timeout")
+		
+		yield(get_tree().create_timer(t_ole),"timeout")
+		
 func _ready():
-	$Path2D.add_child(enemy.instance())
-	yield(get_tree().create_timer(5),"timeout")
-	$Path2D.add_child(enemy.instance())
+	
+	var skeleton = preload("res://Scenes/Skeleton.tscn")
+	_rafaga(skeleton,5,1,2,6)
