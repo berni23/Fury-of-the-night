271eab69dac5943cc291cda09e6e4c6ba7069b2d
Bernat <bernatferrerm@gmail.com>
2019-06-09 19:08:39 +0200

    Monedes fan pampallugues i desapareixen

--- Objects/Coins/Coin1.gd
+++ Objects/Coins/Coin1.gd
@@ -2,10 +2,13 @@ extends Area2D
 
 # class member variables go here, for example:
 # var a = 2
-# var b = "textvar")
+# var b = "textvar"
 
 func _ready():
 	self.add_to_group(Groups.Money)
 
 func _on_Timer_timeout():
 	 $SpriteCoin.animation ="default2"
+
+func _on_Timer2_timeout():
+	self.queue_free()
