fd6178675334500d580e9303fd4a196af825087d
Bernat <bernatferrerm@gmail.com>
2019-06-10 00:02:42 +0200

    " El fang para de moures quan no hi han enemics"

--- Objects/Traps/Mud.gd
+++ Objects/Traps/Mud.gd
@@ -15,21 +15,28 @@ func _on_Area2D_area_entered(area):
 			$AnimatedSprite.play()
 			
 		enemy_slow.append(area.get_parent())
+		print(len(enemy_slow))
 		self.get_node("AudioStreamPlayer2D").play()
 		area.get_parent().speed = area.get_parent().speed/2
 	
 		count+=1
 		
-	
 func _on_Area2D_area_exited(area):
+	
 	
 	if area.get_parent() in enemy_slow:
 		area.get_parent().speed = area.get_parent().speed*2
 		enemy_slow.erase(area.get_parent())
 		
-	if count ==enemies_affected and len(enemy_slow)==0:
+	print(len(enemy_slow))	
+	
+	if len(enemy_slow)==0:
 		
-		self.queue_free()
+		$AnimatedSprite.stop()
+		
+		if count ==enemies_affected :
+		
+			self.queue_free()
 	
 	
 
