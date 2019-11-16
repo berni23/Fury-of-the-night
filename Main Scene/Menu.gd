80c10efe4d63cbb0398eead3783fecc32aeca05f
Bernat Ferrer <bernatferrerm@gmail.com>
2019-11-16 12:55:12 +0100

    "Millora menu"

--- Main Scene/Menu.gd
+++ Main Scene/Menu.gd
@@ -2,18 +2,35 @@ extends Node2D
 
 export (PackedScene) var Main
 
-func _ready():
-	$MarginContainer/VBoxContainer/VBoxContainer2/TextureButton.grab_focus()
+#func _ready():
+	#$MarginContainer/VBoxContainer/VBoxContainer2/TextureButton.grab_focus()
 
-func _physics_process(delta):
-	if $MarginContainer/VBoxContainer/VBoxContainer2/TextureButton.is_hovered()==true:
-		$MarginContainer/VBoxContainer/VBoxContainer2/TextureButton.grab_focus()
-	if $MarginContainer/VBoxContainer/VBoxContainer2/TextureButton2.is_hovered()==true:
-		$MarginContainer/VBoxContainer/VBoxContainer2/TextureButton2.grab_focus()
+#func _physics_process(delta):
+#	if $MarginContainer/VBoxContainer/VBoxContainer2/Button.is_hovered()==true:
+#		$MarginContainer/VBoxContainer/VBoxContainer2/Button.grab_focus()
+#	if $MarginContainer/VBoxContainer/VBoxContainer2/Button2.is_hovered()==true:
+#		$MarginContainer/VBoxContainer/VBoxContainer2/Button2.grab_focus()
+#
+
+
+func _on_Button_pressed():	
+	$start.play()	
 	
-func _on_TextureButton_pressed():
+func _on_Button2_pressed():	
+	$exit.play()
 	
+func _on_Button2_mouse_entered():
+	$mouse2.show()
+	$mouse1.hide()
+	
+	
+func _on_Button_mouse_entered():
+	$mouse1.show()
+	$mouse2.hide()
+
+func _on_start_finished():
 	get_tree().change_scene("res://Main Scene/Main.tscn")
 
-func _on_TextureButton2_pressed():
+
+func _on_exit_finished():
 	get_tree().quit()
