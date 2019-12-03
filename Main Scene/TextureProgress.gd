8956cdad2428fff0e888b7b58d7ccea31156a955
Bernat Ferrer <bernatferrerm@gmail.com>
2019-12-03 21:27:20 +0100

    " Retoc chakra challenges"

--- Main Scene/TextureProgress.gd
+++ Main Scene/TextureProgress.gd
@@ -3,7 +3,8 @@ extends TextureProgress
 var challengeFifty = false
 var challengeTwoHundred = false
 var challengeItems = false
-
+#var i =0
+var c=0
 var items = ["Mud","Friend","Tower1","Tower2","Tower3","Bomb","Shred"]
 
 export (PackedScene) var filled
@@ -21,26 +22,37 @@ func _on_enemy_dead(num):
 		$Label.text = message
 		$Completed.play()
 		$TimerLetters.start()
-		$TimerChallenge.start()
 		challengeFifty = true
-		self.value+=1 # afegir  una escena amb musiqueta i label dien que guanyes un chakra
-		
+		self.value+=1 	
 	elif num>=200 and challengeTwoHundred==false:
 		message = '200 enemy challenge completed!'
 		$Label.text = message
 		$Completed.play()
 		$TimerLetters.start()
-		$TimerChallenge.start()
 		challengeTwoHundred = true
 		self.value +=1
-
-func _on_TimerChallenge_timeout():
-	$Label.visible_characters=0
 func _on_TimerLetters_timeout():
 	$Label.visible_characters+=1
 	if $Label.visible_characters==len(message):
 		$TimerLetters.stop()
-
+		$TimerLetters2.start()
+		
+func _on_TimerLetters2_timeout():
+	c+= 1e-2
+	self.get_node('Label').add_color_override("font_color", Color(0.478,0.8235,0.929,1-c))
+	self.get_node('Label').add_color_override("font_outline_modulate", Color(0.478,0.8235,0.929,1-c))
+	if 1-c<0.05:	
+		$TimerLetters2.stop()
+		self.get_node('Label').add_color_override("font_color", Color(0.478,0.8235,0.929,1))
+		self.get_node('Label').add_color_override("font_outline_modulate", Color(0.478,0.8235,0.929,1))
+		$Label.visible_characters=0
+		c=0
+#	$Label.text[i]=' '
+#	i+=1
+#	if i==len(message):
+#		i=0
+#		$Label.visible_characters=0
+#		$TimerLetters2.stop()
 func item_used(item):
 	if challengeItems ==false:
 		items.erase(item)
@@ -51,12 +63,5 @@ func item_used(item):
 			$TimerChallenge.start()
 			$TimerLetters.start()
 			challengeItems =true
-			self.value +=1
-		
-		else: return
-			
-		
-    
-	
-	
-
+			self.value+=1
+		else: return  
--- Main Scene/prova.gd
+++ Main Scene/prova.gd
@@ -3,18 +3,14 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var items = {"Mud": 0, "Friend":0,"Tower1":0,"Tower2":0,"Tower3":0,"Bomb":0,"Shred":0}
-var list = ["Mud","Tower"]
+var message = 'Hey'
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	items.erase("Mud")
-	print(items)
-	print(items['Tower1'])
-	list.erase("Mud")
-	print(list)
-	list.erase("Mud")
-		
+	print(message[2])
+	
+	
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
