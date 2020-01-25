46e90b01d39618f2317da95d85bb8da64d315259
Marc Roda <marcroda04@gmail.com>
2020-01-25 23:52:39 +0100

    Primera versio funcional de la bala catapulta amb parabola. Falta millorar els visuals

--- Objects/Towers/TowerCanon.gd
+++ Objects/Towers/TowerCanon.gd
@@ -9,13 +9,15 @@ var enemy_range = []
 var next = 'NO'
 
 func _ready():
-	get_tree().get_root().get_node("GameMaster/Chakra").item_used("Tower3")
+	var chakra = get_tree().get_root().get_node("GameMaster/Chakra")
+	if chakra:
+		chakra.item_used("Tower3")
 	add_to_group(Groups.Towers)
 
 func _on_TowerCanon_area_entered(area):
 	if area.get_parent().is_in_group(Groups.Enemies):
 		enemy_range.append(area.get_parent())
-		
+
 func _on_TowerCanon_area_exited(area):
 	if area.get_parent().is_in_group(Groups.Enemies):
 		enemy_range.erase(area.get_parent())
@@ -26,10 +28,13 @@ func  _process(delta):
 		
 func shoot(enemy):
 	var Nbala = bala.instance()
-#	bala.p_0 = self.global_position
-#	bala.p_f = enemy.global_position
-#	bala.speed = self.speed_bala
-	self.add_child(Nbala)	
+	Nbala.initial_pos = self.global_position
+	var enemy_vector = enemy.global_position-self.global_position
+	# These part might need to be modified for better visuals
+	Nbala.initial_speed = 200
+	Nbala.initial_alpha = deg2rad(60)
+	Nbala.initial_beta = enemy_vector.angle()
+	self.add_child(Nbala)
 	reload = false
 	$ReloadTimer.start()
 
---
+++ Objects/Towers/canonBall.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+var t_life = 10
+var damage = 0
+
+# These are provided by the shooting tower on creation
+var initial_pos
+var initial_speed 
+var initial_alpha  # vertical_angle
+var initial_beta  # horizontal_angle
+# These might need tuning
+var g = 98
+var h = 80
+# These are used in the internal computations
+var threeDspeed
+var threeDposition
+
+func _ready():
+	# Assing itself to Bullets group
+	self.add_to_group(Groups.Bullets)
+	# Start the timer to self-destroy at maximum range
+	$LifeTimer.wait_time = t_life
+	$LifeTimer.start()
+	# Set up initial 3d position and speed
+	threeDposition = Vector3(initial_pos[0],initial_pos[1],h)
+	threeDspeed = initial_speed*Vector3(cos(initial_alpha)*cos(initial_beta),
+										cos(initial_alpha)*sin(initial_beta),
+										sin(initial_alpha))
+
+func _process(delta):
+	print(threeDposition)
+	print(threeDspeed)
+	print(global_position)
+	print("=====")
+	# Compute new speed in 3d
+	threeDspeed -= Vector3(0,0,g*delta)
+	# Move in 3d according to speed
+	threeDposition += threeDspeed*delta
+	# Project movement to 2d
+	self.global_position = CustomFunc.project_to_2d(threeDposition,20)
